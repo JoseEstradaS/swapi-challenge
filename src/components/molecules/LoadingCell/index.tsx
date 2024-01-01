217cd2d76c65959e8d2d886c39c344a1664ac165
JoseEstradaS <jose5642260@gmail.com>
2024-01-01 14:28:02 -0500

    chore: LoadingCell component

---
+++ src/components/molecules/LoadingCell/index.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material"
+import LoadingIndicator from "../LoadingIndicator"
+
+
+const LoadingCell = () => {
+  return (
+    <Box margin={2}>
+      <LoadingIndicator />
+    </Box>
+  )
+}
+
+export default LoadingCell
