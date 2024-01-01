47a1a43c5207ed76992ec577b9b685515267881e
JoseEstradaS <jose5642260@gmail.com>
2024-01-01 14:25:29 -0500

    chore: NoticeCell Component

---
+++ src/components/molecules/NoticeCell/index.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material"
+import NoticeText from "../../atoms/NoticeText"
+
+
+const NoticeCell = () => {
+  return (
+    <Box margin={2}>
+      <NoticeText />
+    </Box>
+  )
+}
+
+export default NoticeCell
