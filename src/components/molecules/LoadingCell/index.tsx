962f8554d7b68167bf2be5adec776608a865b649
JoseEstradaS <jose5642260@gmail.com>
2024-01-01 15:41:12 -0500

    chore: centered LoadingCell

--- src/components/molecules/LoadingCell/index.tsx
+++ src/components/molecules/LoadingCell/index.tsx
@@ -4,7 +4,7 @@ import LoadingIndicator from '../LoadingIndicator'
 
 const LoadingCell = () => {
   return (
-    <Box margin={2}>
+    <Box display='flex' justifyContent='center' alignItems='center' margin={2}>
       <LoadingIndicator />
     </Box>
   )
