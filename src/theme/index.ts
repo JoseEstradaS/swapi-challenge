ad5e35f9315a94afdd2a6814638a3821546ed7f0
JoseEstradaS <jose5642260@gmail.com>
2024-01-01 14:06:22 -0500

    chore: added theme and font

---
+++ src/theme/index.ts
@@ -0,0 +1,27 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    error: {
+      main: '#EC5757'
+    },
+    primary: {
+      main: '#121212',
+      light: '#828282',
+      dark: '#333333'
+    }
+  },  
+  typography: {
+    fontFamily: '"SF Pro Display", "Roboto", "Arial", sans-serif',
+    h2: {
+      fontSize: '17px',
+      fontWeight: 700,
+    },
+    body1: {
+      fontSize: '14pt',
+      fontWeight: 400
+    },
+  },
+});
+
+export default theme;
