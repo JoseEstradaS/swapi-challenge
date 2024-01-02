104d7be27dfa42250b4399f9064bfe8a985b4170
JoseEstradaS <jose5642260@gmail.com>
2024-01-02 12:05:01 -0500

    feat: Header now support selectedPerson and onBack action

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -1,13 +1,24 @@
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import { AppBar, Toolbar, Typography } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Person } from '../../../types';
+import { IconButton } from '@mui/material';
 
-const Header = () => {
+interface IHeader {
+  selectedPerson?: Person;
+  onBack: () => void;
+}
+
+const Header = ({onBack, selectedPerson}: IHeader) => {
   return (
     <AppBar position="sticky">
       <Toolbar>
+        { selectedPerson && (
+          <IconButton color='inherit' onClick={onBack}>
+            <ArrowBackIcon />
+          </IconButton>
+        )}
         <Typography variant="h6" component="div">
-          Ravn Star Wars Registry
+          { selectedPerson?.name ? selectedPerson.name : 'Ravn Star Wars Registry'}
         </Typography>
       </Toolbar>
     </AppBar>
