42a560055e28416d0db4a6529b2e624a4b085da8
JoseEstradaS <jose5642260@gmail.com>
2024-01-03 10:17:31 -0500

    test: updated test to support new prop on Header

--- src/components/organisms/Header/index.test.tsx
+++ src/components/organisms/Header/index.test.tsx
@@ -5,7 +5,7 @@ import Header from '.';
 
 describe('Header component', () => {
   it('renders with default title when no person is selected', () => {    
-    const { container } = render(<Header onBack={() => {}} />);
+    const { container } = render(<Header isMobile onBack={() => {}} />);
 
     const backButton = container.querySelector('button[aria-label="Go back"]');
     const titleElement = container.querySelector('header > div > div');
@@ -18,7 +18,7 @@ describe('Header component', () => {
   it('renders with person name and back button when a person is selected', () => {
     const selectedPerson = { name: 'Luke Skywalker' } as Person;
     
-    const { container } = render(<Header selectedPerson={selectedPerson} onBack={() => {}} />);
+    const { container } = render(<Header isMobile selectedPerson={selectedPerson} onBack={() => {}} />);
 
     const backButton = container.querySelector('button[aria-label="Go back"]');
     const titleElement = container.querySelector('header > div > div');
